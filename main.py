8f0cce2a8ca9de0e81b0adcd10148bab4e146879
Himangshu Paul <153427109+devhimangshu@users.noreply.github.com>
2026-04-18 10:45:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,10 +28,11 @@ if not BOT_TOKEN:
     logger.error("❌ BOT_TOKEN not found! Please set your bot token in environment variables.")
     sys.exit(1)
 
-REQUIRED_CHANNELS = ["@your channel username"]
-GROUP_JOIN_LINK = "https://t.me/your_group_link"
-OWNER_ID = your tg user id (integer)    #Example: 6282811167
-OWNER_USERNAME = "@your username"
+BOT_TOKEN = "8580065060:AAENuJLKHQF2X_sqxv79UK8It3JwbHG_nrE"
+REQUIRED_CHANNELS = ["@topupkingfflikes"]
+GROUP_JOIN_LINK = "t.me/topupkinglikegroup"
+OWNER_ID = 1974829934
+OWNER_USERNAME = "@topup_king"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 like_tracker = {}   # in-memory cache
